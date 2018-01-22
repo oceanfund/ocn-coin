052a1bf6dbd840833e539569b35981ea8b4396a1
Administrator <admin@neuroboticsinc.io>
2018-01-22 20:26:47 +0100

    app script

---
+++ app.sh
@@ -0,0 +1,4 @@
+npm install --save deck.gl luma.gl
+npm install
+
+export MapboxAccessToken={pk.eyJ1IjoiY3lyaWxsdXR0ZXJvZHQiLCJhIjoiY2pjcWtxcGRoMGYybTJybzJkbjVzeGdzdCJ9.fbcG6ubIoQnRypab9qUQCQ} && npm start
