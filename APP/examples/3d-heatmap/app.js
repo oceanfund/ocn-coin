9a32e32c3c8340decac9462dca2122d04ff77d0c
Spottybadrabbit <cyril.lutterodt@mavs.uta.edu>
2018-01-24 23:54:04 +0100

    Updates

--- APP/examples/3d-heatmap/app.js
+++ APP/examples/3d-heatmap/app.js
@@ -3,33 +3,34 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import MapGL from 'react-map-gl';
 import DeckGLOverlay from './deckgl-overlay.js';
-
+import collect from '@turf/collect';
+import { slide as Menu } from 'react-burger-menu'
 import {csv as requestCsv} from 'd3-request';
 
 // Set your mapbox token here
 //const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiY3lyaWxsdXR0ZXJvZHQiLCJhIjoiY2pjcWtxcGRoMGYybTJybzJkbjVzeGdzdCJ9.fbcG6ubIoQnRypab9qUQCQ';// eslint-disable-line
 
-// Source data CSV
-//const DATA_URL =
-  'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'; // eslint-disable-line
-
-const DATA_URL = '/latlng.csv'
+const DATA_URL = '/Palau_geocordinates_final.csv'
 // Source data CSV
 //const DATA_URL = 'file:///Users/neurobotics/ocn-coin/deck.gl-master/examples/react/OCN/heatmap-data.csv'; // eslint-disable-line
 
 
-class Root extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewport: {
-        ...DeckGLOverlay.defaultViewport,
-        width: 500,
-        height: 500
-      },
-      data: null
-    };
+
+    class Root extends Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          viewport: {
+            ...DeckGLOverlay.defaultViewport,
+            width: 500,
+            height: 500
+          },
+          data: null
+        };
+    const { viewport, data } = this.state;
+
+    //collect(points, polys, 'population', 'populationValues');
 
     requestCsv(DATA_URL, (error, response) => {
       if (!error) {
@@ -59,18 +60,22 @@ class Root extends Component {
 
   render() {
     const {viewport, data} = this.state;
-
     return (
       <MapGL
         {...viewport}
-        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapStyle="mapbox://styles/mapbox/satellite-v9"
+        //mapStyle="mapbox://styles/mapbox/dark-v9"
         onViewportChange={this._onViewportChange.bind(this)}
         mapboxApiAccessToken={MAPBOX_TOKEN}
       >
         <DeckGLOverlay viewport={viewport} data={data || []} />
       </MapGL>
     );
+     
+      
+    
+    }
   }
-}
+
 
 render(<Root />, document.body.appendChild(document.createElement('div')));
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 
+This is a minimal standalone version of the 3D VIsualization of ethereum contract of OCN coin  example
+
+### Usage
+- Copy the content of this folder to your project.
+
 Type to run:
+```
+npm install
+
 cd ./...
 npm install --save deck.gl luma.gl
 npm install --save react@0.14.5
@@ -7,6 +15,21 @@ react-native upgrade
 npm install --save 'react@^15'.
 npm install mapbox-gl --save
 npm install -g install-peerdeps
-npm build
-npm start
+npm install -g truffle
+truffle unbox webpack
+truffle develop
+compile
+migrate
+npm run dev
+truffle test
+truffle compile
+truffle migrate
+truffle migrate --network ropsten
+cd /ocn-coin/APP/examples/3d-heatmap/
+
 export MapboxAccessToken={pk.eyJ1IjoiY3lyaWxsdXR0ZXJvZHQiLCJhIjoiY2pjcWtxcGRoMGYybTJybzJkbjVzeGdzdCJ9.fbcG6ubIoQnRypab9qUQCQ} && npm start
+```
+
+
+### Data format
+Sample data is stored in [as a long and lat and eventually moved to a JSON file that count fishes, pH, Oxidation and temprature]
